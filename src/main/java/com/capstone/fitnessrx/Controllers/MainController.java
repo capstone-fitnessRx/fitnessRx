fd359b45a403b310709d1f6cecbc9b3f022497a1
Austin Joiner <austin.j.joiner1@gmail.com>
2023-06-02 16:04:31 -0400

    added controller endpoints

--- src/main/java/com/capstone/fitnessrx/Controllers/MainController.java
+++ src/main/java/com/capstone/fitnessrx/Controllers/MainController.java
@@ -1,16 +1,139 @@
 package com.capstone.fitnessrx.Controllers;
 
-import com.capstone.fitnessrx.Repositories.UserRepository;
+import com.capstone.fitnessrx.Repositories.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MainController {
 
-
-
     private final UserRepository userDao;
+    private final CommentsRepository commentsDao;
+    private final FriendsRepository firendsDao;
+    private final MessagesRepository messagesDao;
+    private final RatingsRepository ratingsDao;
+    private final CalenderRepository calenderDao;
+    private final WorkoutRepository workoutDao;
+    private Model model;
 
-    public MainController(UserRepository userDao) {
+
+    public MainController(UserRepository userDao, CommentsRepository commentsDao, FriendsRepository firendsDao, MessagesRepository messagesDao, RatingsRepository ratingsDao, CalenderRepository calenderDao, WorkoutRepository workoutDao) {
         this.userDao = userDao;
+        this.commentsDao = commentsDao;
+        this.firendsDao = firendsDao;
+        this.messagesDao = messagesDao;
+        this.ratingsDao = ratingsDao;
+        this.calenderDao = calenderDao;
+        this.workoutDao = workoutDao;
+    }
+
+
+
+
+    @GetMapping("/home")
+
+    public String getHome(Model model) {
+
+
+
+        return "index/landingpage";
+    }
+
+    @GetMapping("/profile/{id}")
+    public String getProfile(@PathVariable Long id, Model model) {
+
+
+
+        return "index/profile";
+    }
+
+    @GetMapping("/calender")
+    public String getCalender() {
+
+
+
+        return "index/calender";
+    }
+
+    @GetMapping("/my-workouts/{id}")
+
+    public String getMyWorkouts(@PathVariable String id) {
+
+
+
+        return "index/myworkouts";
+    }
+
+    @GetMapping("/map")
+
+    public String getMap() {
+
+
+        return "index/map";
+    }
+
+    @GetMapping("/feed")
+
+    public String getFeed(Model model) {
+
+
+
+        return "index/feed";
+    }
+
+    @GetMapping("/favorites")
+
+    public String getFavorites(Model model) {
+
+
+
+        return "index/favorites";
+    }
+
+    @GetMapping("/workout-builder")
+
+    public String getBuilder(Model model) {
+
+
+
+        return "index/workoutbuilder";
+    }
+
+    @GetMapping("/exercise-page")
+
+    public String getExercise(Model model) {
+
+
+
+        return "index/exercise";
+    }
+
+    @GetMapping("/exercise-display")
+
+    public String getExerciseDisplay(Model model) {
+
+
+
+        return "index/specificexercise";
+    }
+
+    @GetMapping("/workouts-wall")
+
+    public String getWorkoutWall(Model model) {
+
+
+
+        return "index/workouts";
+    }
+
+    @GetMapping("/workout-plan")
+
+    public String getWorkoutPlan(Model model) {
+
+
+
+        return "index/workoutplan";
     }
 }
