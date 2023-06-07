73ad6507f59d0d3534641d0e710c32f31817b0aa
David <davidbanker22@yahoo.com>
2023-06-07 13:36:08 -0500

    fixed js connectivity

--- src/main/resources/static/js/exerciseDB.js
+++ src/main/resources/static/js/exerciseDB.js
@@ -261,16 +261,24 @@ $(document).ready(function () {
             let id = data[i].id;
             let name = data[i].name;
             let target = data[i].target;
-            card += '<form class="col-md-4 col-lg-3 col-xl-2" method="post">\n' +
+            card += '<form class="col-md-4 col-lg-3 col-xl-2" th:action="@{/exercise-display}" th:method="post" th:object="${post}"> \n' +
                 '        <button class="btn " type="submit">\n' +
-                '            <div class="card text-center" style="width: 158px">\n' +
-                '                <div class="card-header " style="height: 115px">\n' +
+                '            <div class="card text-center" style="width: 180px">\n' +
+                '                <div class="card-header " style="height: 130px">\n' +
                 '                    <h5 class="card-title my-auto">'+name+'</h5>\n' +
                 '                </div>\n' +
                 '                <div class="card-body" id=>\n' +
-                '                    <img class="card-text">image will go here</img>\n' +
+                '                    <img src="'+gifUrl+'" style="" class="img-fluid" alt="">\n' +
                 '                </div>\n' +
                 '            </div>\n' +
+                <!--            sends the name of the exercise-->
+                '            <input class="d-none form-control" type="text" name="exerciseName" value="'+name+'">\n' +
+                <!--             sends the muscles targeted-->
+                '            <input class="d-none form-control" type="text" name="exerciseTarget" value="'+target+'">\n' +
+                <!--            sends the equipment-->
+                '            <input class="d-none form-control" type="text" name="exerciseEquipment" value="'+equipment+'">\n' +
+                <!--            sends the gif-->
+                '            <input class="d-none form-control" type="text" name="exerciseGif" value="'+gifUrl+'">\n' +
                 '        </button>\n' +
                 '    </form>'
             // exercises += '<option id="' + id + '" class="exercises" value="' + id + '">' +
